25910f9edb98a50318cbf7f0917c5df2ca8004d9
Dave Collison <davecollison@BaseMac.local>
2020-07-06 12:09:52 +0100

    create headings and map months from 1-12 to 0-11

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hi")
+const data = d3.json("global-temperature.json").then(data => {
+    heatMap( data )
+})
+
+function heatMap( data ) {
+    const { baseTemperature, monthlyVariance } = data
+
+    const root = d3.select("body")
+    
+    monthlyVariance.map( d => d.month--)
+
+    root.append("h1")
+    .attr("id","title")
+    .text("Monthly Global Land-Surface Temperature")
+
+    root.append("h3")
+    .attr("id","description")
+    .text(`${monthlyVariance[0].year} - ${monthlyVariance[monthlyVariance.length - 1].year}: Base Temperature ${ baseTemperature }˚C`)
+}
